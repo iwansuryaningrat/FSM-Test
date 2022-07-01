17afa8ced80704ec94cfe6faa282f04f226e3ba4
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-01 10:40:01 +0700

    Add UI

---
+++ app/Views/responses.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/styles/styles.css">
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+
+    <title>Responses</title>
+</head>
+
+<body class="container-fluid bg-gradient p-5 pt-2">
+    <h1 class="text-center text-white">Responses</h1>
+
+    <div class="text-center">
+        <div class="row mb-3">
+            <h4 class="col text-left d-inline text-white">Topics Concurency</h4>
+            <div class="col text-right d-inline text-white" href="">
+                <a class="btn btn-green rounded text-white pl-4 pr-4 p-2" href="">Add Response</a>
+            </div>
+        </div>
+
+        <div class="container-fluid bg-white rounded p-3">
+            <table class="table-borderless table-striped table">
+                <thead>
+                    <tr class="row">
+                        <th class="col">No</th>
+                        <th class="col">What is Concurency</th>
+                        <th class="col">Description</th>
+                        <th class="col">Attempt</th>
+                        <th class="col">User</th>
+                        <th class="col">Action</th>
+                    </tr>
+                </thead>
+
+                <!-- table content -->
+
+                <!-- table row -->
+                <tr class="row">
+                    <td class="col">1</td>
+                    <td class="col">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                    <td class="col">Mauris tortor augue, varius quis pulvinar sit amet, efficitur in elit. Vestibulum
+                        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse auctor
+                        velit ac urna porta tristique. Pellentesque dignissim congue venenatis. In suscipit rhoncus
+                        dignissim. Nunc malesuada porta ligula, quis maximus enim congue ac.</td>
+                    <td class="col">2</td>
+                    <td class="col">iwan</td>
+                    <td class="col">
+                        <div class="container-fluid">
+                            <a class="btn btn-blue rounded text-white d-block m-3" href="#">Update</a>
+                            <a class="btn btn-red rounded text-white d-block m-3" href="#">Delete</a>
+                        </div>
+
+                    </td>
+                </tr>
+
+                <!-- table row -->
+                <tr class="row">
+                    <td class="col">2</td>
+                    <td class="col">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                    <td class="col">Mauris tortor augue, varius quis pulvinar sit amet, efficitur in elit. Vestibulum
+                        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse auctor
+                        velit ac urna porta tristique. Pellentesque dignissim congue venenatis. In suscipit rhoncus
+                        dignissim. Nunc malesuada porta ligula, quis maximus enim congue ac.</td>
+                    <td class="col">5</td>
+                    <td class="col">iwan</td>
+                    <td class="col">
+                        <div class="container-fluid">
+                            <a class="btn btn-blue rounded text-white d-block m-3" href="#">Update</a>
+                            <a class="btn btn-red rounded text-white d-block m-3" href="#">Delete</a>
+                        </div>
+
+                    </td>
+                </tr>
+
+
+
+            </table>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
---
+++ app/Views/topics.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/styles/styles.css">
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+
+    <title>Topics</title>
+</head>
+
+<body class="container-fluid bg-gradient p-5">
+    <h1 class="text-center text-white pb-5">Topics</h1>
+    <div class="text-center">
+        <div class="container-fluid bg-white rounded p-3">
+            <div class="row">
+                <div class="col-12 font-weight-bold">Topics</div>
+            </div>
+            <hr>
+            <table class="table table-striped table-borderless">
+                <thead>
+                    <tr class="row">
+                        <th class="col">No</th>
+                        <th class="col">Name</th>
+                        <th class="col">Action</th>
+                    </tr>
+                </thead>
+
+
+                <!-- table content -->
+
+                <!-- table row -->
+                <tr class="row">
+                    <td class="col">1</td>
+                    <td class="col">Concurency</td>
+                    <td class="col"><a class="btn btn-blue rounded text-white" href="/responses/">Open Form</a></td>
+                </tr>
+
+                <!-- table row -->
+                <tr class="row">
+                    <td class="col">2</td>
+                    <td class="col">Web Dev</td>
+                    <td class="col"><a class="btn btn-blue rounded text-white" href="/responses/">Open Form</a></td>
+                </tr>
+
+            </table>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
